b1260749a9d350e0c1711ed824737e59eb9846da
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-09-04 05:15:29 +0300

    fix: initial state fix

--- lab1/scripts/setSourceCode.js
+++ lab1/scripts/setSourceCode.js
@@ -1,16 +1,5 @@
 const url = "https://raw.githubusercontent.com/lipa44/web-labs/lab-1/lab1/index.html";
 
-let codeSnippet = document.getElementById("code-snippet");
-if (!codeSnippet) {
-    const Http = new XMLHttpRequest();
-    Http.open("GET", url);
-    Http.send();
-
-    Http.onreadystatechange = (e) => {
-        document.getElementById("code-snippet").innerText = Http.responseText;
-    }
-}
-
 function setSourceCode() {
     const Http = new XMLHttpRequest();
     Http.open("GET", url);
