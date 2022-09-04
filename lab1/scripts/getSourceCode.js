bc53ea927c24d52b2af89c44972a670a14077a31
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-09-04 05:05:33 +0300

    style: some changes

--- lab1/scripts/getSourceCode.js
+++ lab1/scripts/getSourceCode.js
@@ -1,10 +1,30 @@
 const url = "https://raw.githubusercontent.com/lipa44/web-labs/lab-1/lab1/index.html";
 
-const Http = new XMLHttpRequest();
-Http.open("GET", url);
-Http.send();
+let codeSnippet = document.getElementById("code-snippet");
+if (!codeSnippet) {
+    const Http = new XMLHttpRequest();
+    Http.open("GET", url);
+    Http.send();
 
-Http.onreadystatechange = (e) => {
-    console.log(Http.responseText);
-    document.getElementById("code-snippet").innerText = Http.responseText;
+    Http.onreadystatechange = (e) => {
+        document.getElementById("code-snippet").innerText = Http.responseText;
+    }
+}
+
+function setSourceCode() {
+    const Http = new XMLHttpRequest();
+    Http.open("GET", url);
+    Http.send();
+
+    let codeSnippet = document.getElementById("code-snippet");
+    let mainIntro = document.getElementById("main__intro");
+
+    Http.onreadystatechange = (e) => {
+        Http.responseText;
+        mainIntro.style.display = "block";
+        codeSnippet.style.display = "block";
+        codeSnippet.style.padding = "2rem";
+        codeSnippet.innerText = Http.responseText;
+        codeSnippet.style.fontSize = "medium";
+    }
 }
