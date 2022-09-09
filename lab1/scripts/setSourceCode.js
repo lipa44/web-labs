d65256298017aa5de9476ff8975a2849df6b0e0d
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-09-09 18:43:17 +0300

    feat: promise used for http request

--- lab1/scripts/setSourceCode.js
+++ lab1/scripts/setSourceCode.js
@@ -1,23 +1,24 @@
 const url = "https://raw.githubusercontent.com/lipa44/web-labs/lab-1/lab1/index.html";
+const fetchPromise = fetch(url);
 
-function setSourceCode() {
-    const Http = new XMLHttpRequest();
-    Http.open("GET", url);
-    Http.send();
+setSourceCode = () =>
+    fetch(url)
+        .then(response => {
+            return response.text();
+        })
+        .then((res) => {
+            console.log(res);
 
-    let codeSnippet = document.getElementById("main__code-snippet");
-    let codeSnippet_code = document.getElementById("code-snippet__code");
-    let mainMock = document.getElementById("main__body-mock");
-    let asciiArt = document.getElementById("main__ascii-art");
-    let mainIntro = document.getElementById("main__intro");
+            let codeSnippet = document.getElementById("main__code-snippet");
+            let codeSnippet_code = document.getElementById("code-snippet__code");
+            let mainMock = document.getElementById("main__body-mock");
+            let asciiArt = document.getElementById("main__ascii-art");
+            let mainIntro = document.getElementById("main__intro");
 
-    Http.onreadystatechange = (e) => {
-        Http.responseText;
-        mainMock.style.display = "none";
-        asciiArt.style.display = "none";
+            mainMock.style.display = "none";
+            asciiArt.style.display = "none";
 
-        mainIntro.style.display = "block";
-        codeSnippet.style.display = "flex";
-        codeSnippet_code.innerText = Http.responseText;
-    }
-}
+            mainIntro.style.display = "block";
+            codeSnippet.style.display = "flex";
+            codeSnippet_code.innerText = res;
+        })
