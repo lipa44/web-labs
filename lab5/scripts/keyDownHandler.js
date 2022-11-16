e841c93e045dd45cffc33a5d6ea04a8c4cd82dc8
Mikhail Libchenko <libchenkomikhail@gmail.com>
2022-11-16 20:11:24 +0300

    feat: copy handler added

--- lab5/scripts/keyDownHandler.js
+++ lab5/scripts/keyDownHandler.js
@@ -1,6 +1,16 @@
 import {handleBackspace, handleDownArrow, handleEnter, handleShiftEnter, handleUpArrow} from "./keyHandlers.js";
 
-const Keys = {ENTER: 13, BACK_SPACE: 8, LEFT_ARROW: 37, UP_ARROW: 38, RIGHT_ARROW: 39, DOWN_ARROW: 40};
+const Keys = {
+    ENTER: 13,
+    BACK_SPACE: 8,
+    LEFT_ARROW: 37,
+    UP_ARROW: 38,
+    RIGHT_ARROW: 39,
+    DOWN_ARROW: 40,
+    C_KEY: 67,
+    V_KEY: 86,
+    X_KEY: 88
+};
 
 const keyDownHandler = (e) => {
     const curParagraph = e.target
@@ -11,12 +21,13 @@ const keyDownHandler = (e) => {
     const isLineEnd = focusOffset === textLength;
     const curNodeIndex = [...curParagraph.childNodes].indexOf(selection.focusNode);
 
+    // console.log("Is copy-paste: " + ifCopyPasteOperation(e))
+
     switch (e.keyCode) {
         case Keys.ENTER: // создаём новый параграф или новую строчку в нём
             if (e.shiftKey) {
                 handleShiftEnter(e, curParagraph, selection)
-            }
-            else {
+            } else {
                 handleEnter(e, curParagraph, keyDownHandler)
             }
 
@@ -34,7 +45,38 @@ const keyDownHandler = (e) => {
             handleUpArrow(e, curParagraph, curNodeIndex, focusOffset, isLineStart, selection);
 
             break;
+
+        // default:
+        //     if (!ifCopyPasteOperation(e)) return;
+        //
+        //     switch (e.keyCode) {
+        //         case Keys.C_KEY:
+        //             break;
+        //
+        //         case Keys.V_KEY:
+        //             break;
+        //
+        //         case Keys.X_KEY:
+        //             break;
+        //     }
     }
 }
+
+// const ifCopyPasteOperation = (e) => {
+//     e = e || window.event // IE support
+//     const key = e.keyCode
+//     const ctrlDown = e.ctrlKey || e.metaKey // Mac support
+//
+//     // Check for Alt+Gr
+//     if (ctrlDown && e.altKey) return false
+//
+//     // Check for ctrl+c, v and x
+//     else if (ctrlDown && key === Keys.C_KEY) return true // c
+//     else if (ctrlDown && key === Keys.V_KEY) return true // v
+//     else if (ctrlDown && key === Keys.X_KEY) return true // x
+//
+//     // Otherwise false
+//     return false
+// }
 
 export {keyDownHandler}
--- lab5/scripts/onPageLoad.js
+++ lab5/scripts/onPageLoad.js
@@ -8,8 +8,19 @@ const addKeyDownHandler = () => {
 
 const addTemplate = () => loadTemplate();
 
+const addCopyHandler = () => {
+    const editableElements = document.querySelectorAll('[contenteditable]')
+
+    editableElements.forEach(elem => {
+        elem.addEventListener('copy', (e) => {
+            e.clipboardData.setData('text/plain', e.target.innerText);
+            e.preventDefault();
+        })
+    })
+}
 
 (() => {
     addKeyDownHandler()
     addTemplate()
+    addCopyHandler()
 })()
